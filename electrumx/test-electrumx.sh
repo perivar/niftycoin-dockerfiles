e48f17adf57f0f9da563ce832b05b87f4058d3e8
perivar <perivar@nerseth.com>
2021-04-17 14:54:00 +0200

    electrumx fixes

---
+++ electrumx/test-electrumx.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo docker exec -it electrumx electrumx_rpc query --limit 5 NUcBvW67GEqi8CXYPcL4Y5qzw7Vf9rp7wg
---
+++ rpc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo docker exec -it container-niftynode-rpc bash
